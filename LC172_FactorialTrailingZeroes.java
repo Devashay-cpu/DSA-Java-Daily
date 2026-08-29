d3a9406e8558fca4c0c982a4aea05fcdd148fa4e
Devashay-cpu <dewashya0512@gmail.com>
2026-08-29 23:57:36 +0530

    Add LC 172 Factorial Trailing Zeroes

---
+++ LC172_FactorialTrailingZeroes.java
@@ -0,0 +1,19 @@
+class LC172_FactorialTrailingZeroes {
+
+    public static int trailingZeroes(int n) {
+        int count = 0;
+
+        while (n > 0) {
+            n /= 5;
+            count += n;
+        }
+
+        return count;
+    }
+
+    public static void main(String[] args) {
+        int n = 25;
+
+        System.out.println(trailingZeroes(n));
+    }
+}
