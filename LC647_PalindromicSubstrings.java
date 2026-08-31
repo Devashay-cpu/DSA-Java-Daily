cc46a31c34399fc7dee3e497a2667a2f0565a6bd
Devashay-cpu <dewashya0512@gmail.com>
2026-08-31 23:54:05 +0530

    Add LC 647 Palindromic Substrings

---
+++ LC647_PalindromicSubstrings.java
@@ -0,0 +1,34 @@
+class LC647_PalindromicSubstrings {
+
+    public static int countSubstrings(String s) {
+        int count = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            count += expandAroundCenter(s, i, i);       // Odd length
+            count += expandAroundCenter(s, i, i + 1);   // Even length
+        }
+
+        return count;
+    }
+
+    private static int expandAroundCenter(String s, int left, int right) {
+        int count = 0;
+
+        while (left >= 0 && right < s.length()
+                && s.charAt(left) == s.charAt(right)) {
+
+            count++;
+            left--;
+            right++;
+        }
+
+        return count;
+    }
+
+    public static void main(String[] args) {
+
+        String s = "aaa";
+
+        System.out.println(countSubstrings(s));
+    }
+}
