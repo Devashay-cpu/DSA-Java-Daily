9c290042f5f2c367dd23b467037055cfa6befb15
Devashay-cpu <dewashya0512@gmail.com>
2026-09-11 23:32:08 +0530

    LC347_TopKFrequentElements.java

---
+++ LC347_TopKFrequentElements.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+
+        // Count frequency of each number
+        Map<Integer, Integer> freq = new HashMap<>();
+
+        for (int num : nums) {
+            freq.put(num, freq.getOrDefault(num, 0) + 1);
+        }
+
+        // Min Heap based on frequency
+        PriorityQueue<Integer> minHeap =
+                new PriorityQueue<>((a, b) -> freq.get(a) - freq.get(b));
+
+        for (int num : freq.keySet()) {
+            minHeap.offer(num);
+
+            if (minHeap.size() > k) {
+                minHeap.poll();
+            }
+        }
+
+        // Store result
+        int[] result = new int[k];
+
+        for (int i = k - 1; i >= 0; i--) {
+            result[i] = minHeap.poll();
+        }
+
+        return result;
+    }
+}
